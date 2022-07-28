efd469ab84833648701c7a34ac8543f32b1e1406
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-07-28 13:51:12 +0300

    added native handler

--- contracts/handlers/NativeHandler.sol
+++ contracts/handlers/NativeHandler.sol
@@ -9,6 +9,26 @@ abstract contract NativeHandler is INativeHandler {
         payable
         override
     {
-        // TODO
+        require(msg.value > 0, "NativeHandler: zero value");
+
+        emit DepositedNative(msg.value, receiver_, network_);
+    }
+
+    function _withdrawNative(uint256 amount_, address receiver_) internal {
+        require(amount_ > 0, "NativeHandler: amount is zero");
+
+        (bool sent_, bytes memory data_) = payable(receiver_).call{value: amount_}("");
+
+        require(sent_, "NativeHandler: can't send eth");
+    }
+
+    function getNativeSignHash(
+        uint256 amount_,
+        address receiver_,
+        bytes32 txHash_,
+        uint256 txNonce_,
+        uint256 chainId_
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(amount_, receiver_, txHash_, txNonce_, chainId_));
     }
 }
