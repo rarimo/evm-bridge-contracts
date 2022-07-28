39db74c04f2361a020a767873064717be4ad6edb
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-07-28 12:25:44 +0300

    added erc721 handler

---
+++ contracts/interfaces/tokens/IERC721MintableBurnable.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+interface IERC721MintableBurnable is IERC721 {
+    function mintTo(address receiver_, uint256 tokenId_) external returns (bool);
+
+    function burnFrom(address payer_, uint256 tokenId_) external returns (bool);
+}
