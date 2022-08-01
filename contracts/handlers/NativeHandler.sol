6312031b14f3ad15c7e82c6086c993448822140c
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-08-01 17:34:49 +0300

    fixes

--- contracts/handlers/NativeHandler.sol
+++ contracts/handlers/NativeHandler.sol
@@ -4,20 +4,21 @@ pragma solidity ^0.8.9;
 import "../interfaces/handlers/INativeHandler.sol";
 
 abstract contract NativeHandler is INativeHandler {
-    function depositNative(string calldata receiver_, string calldata network_)
-        external
-        payable
-        override
-    {
+    function depositNative(
+        string calldata receiver_,
+        string calldata network_,
+        address token_
+    ) external payable override {
         require(msg.value > 0, "NativeHandler: zero value");
 
-        emit DepositedNative(msg.value, receiver_, network_);
+        emit DepositedNative(msg.value, receiver_, network_, token_);
     }
 
     function _withdrawNative(uint256 amount_, address receiver_) internal {
         require(amount_ > 0, "NativeHandler: amount is zero");
+        require(receiver_ != address(0), "NativeHandler: receiver is zero");
 
-        (bool sent_, bytes memory data_) = payable(receiver_).call{value: amount_}("");
+        (bool sent_, ) = payable(receiver_).call{value: amount_}("");
 
         require(sent_, "NativeHandler: can't send eth");
     }
--- contracts/interfaces/handlers/INativeHandler.sol
+++ contracts/interfaces/handlers/INativeHandler.sol
@@ -2,7 +2,11 @@
 pragma solidity ^0.8.9;
 
 interface INativeHandler {
-    event DepositedNative(uint256 tokenAmount, string receiver, string network);
+    event DepositedNative(uint256 tokenAmount, string receiver, string network, address token_);
 
-    function depositNative(string calldata receiver_, string calldata network_) external payable;
+    function depositNative(
+        string calldata receiver_,
+        string calldata network_,
+        address token_
+    ) external payable;
 }
