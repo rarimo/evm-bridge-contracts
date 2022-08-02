3d54dce65ded861a945271a8453b81c609f12144
Artem Chystiakov <artem.ch31@gmail.com>
2022-08-02 16:06:22 +0300

    added signers threshold

--- contracts/utils/Signers.sol
+++ contracts/utils/Signers.sol
@@ -9,20 +9,49 @@ abstract contract Signers is OwnableUpgradeable {
     using ECDSA for bytes32;
     using EnumerableSet for EnumerableSet.AddressSet;
 
+    uint256 public signaturesThreshold;
+
     EnumerableSet.AddressSet internal _signers;
 
-    function __Signers_init(address[] calldata signers_) public onlyInitializing {
+    function __Signers_init(address[] calldata signers_, uint256 signaturesThreshold_)
+        public
+        onlyInitializing
+    {
         __Ownable_init();
 
         addSigners(signers_);
+        setSignaturesThreshold(signaturesThreshold_);
+    }
+
+    function _checkCorrectSigners(address[] memory signers_) private view {
+        for (uint256 i = 0; i < signers_.length; i++) {
+            for (uint256 j = i + 1; j < signers_.length; j++) {
+                require(signers_[i] != signers_[j], "Signers: duplicate signers");
+            }
+
+            require(_signers.contains(signers_[i]), "Signers: invalid signer");
+        }
+
+        require(signers_.length >= signaturesThreshold, "Signers: threshold is not met");
     }
 
     function _checkSignatures(bytes32 signHash_, bytes[] calldata signatures_) internal view {
-        for (uint256 i = 0; i < signatures_.length; i++) {
-            address recovered_ = signHash_.toEthSignedMessageHash().recover(signatures_[i]);
+        address[] memory signers_ = new address[](signatures_.length);
 
-            require(_signers.contains(recovered_), "Signers: invalid signer");
+        for (uint256 i = 0; i < signatures_.length; i++) {
+            signers_[i] = signHash_.toEthSignedMessageHash().recover(signatures_[i]);
         }
+
+        _checkCorrectSigners(signers_);
+    }
+
+    function setSignaturesThreshold(uint256 signaturesThreshold_) public onlyOwner {
+        require(
+            signaturesThreshold_ > 0 && signaturesThreshold_ <= _signers.length(),
+            "Signers: invalid threshold"
+        );
+
+        signaturesThreshold = signaturesThreshold_;
     }
 
     function addSigners(address[] calldata signers_) public onlyOwner {
