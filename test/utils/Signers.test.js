1a5a80af3a6588481d0116f74dabd9d218d5119e
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-08-03 19:19:15 +0300

    added tests for Signers

--- test/utils/Signers.test.js
+++ test/utils/Signers.test.js
@@ -2,9 +2,156 @@ const { assert } = require("chai");
 const { toBN, accounts, wei } = require("../../scripts/helpers/utils");
 const truffleAssert = require("truffle-assertions");
 const { artifacts, web3 } = require("hardhat");
+const ethSigUtil = require("@metamask/eth-sig-util");
 
 const Signers = artifacts.require("SignersMock");
 
 Signers.numberFormat = "BigNumber";
 
-describe("Signers", () => {});
+let signers;
+
+const OWNER_PRIVATE_KEY = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+
+describe("Signers", () => {
+  beforeEach("setup", async () => {
+    signers = await Signers.new();
+    await signers.__SignersMock_init([await accounts(0)], "1");
+  });
+
+  describe("setSignaturesThreshold", () => {
+    it("should set signaturesThreshold", async () => {
+      let expectedSignaturesThreshold = 1;
+
+      await signers.setSignaturesThreshold(expectedSignaturesThreshold);
+
+      assert.equal(expectedSignaturesThreshold, await signers.signaturesThreshold());
+    });
+
+    it("should revert when try to set signaturesThreshold to 0", async () => {
+      let expectedSignaturesThreshold = 0;
+
+      await truffleAssert.reverts(
+        signers.setSignaturesThreshold(expectedSignaturesThreshold),
+        "Signers: invalid threshold"
+      );
+    });
+  });
+
+  describe("addSigners", () => {
+    it("should add signers", async () => {
+      let expectedSigners = [await accounts(0), await accounts(1), await accounts(2)];
+
+      await signers.addSigners(expectedSigners);
+
+      assert.deepEqual(expectedSigners, await signers.getSigners());
+    });
+
+    it("should revert when try add zero address signer", async () => {
+      let expectedSigners = [await accounts(0), "0x0000000000000000000000000000000000000000", await accounts(2)];
+
+      await truffleAssert.reverts(signers.addSigners(expectedSigners), "Signers: zero signer");
+    });
+  });
+
+  describe("removeSigners", () => {
+    it("should remove signers", async () => {
+      let signersToAdd = [await accounts(0), await accounts(1), await accounts(2)];
+      let signersToRemove = [await accounts(0), await accounts(2)];
+
+      await signers.addSigners(signersToAdd);
+      await signers.removeSigners(signersToRemove);
+
+      assert.deepEqual(await signers.getSigners(), [await accounts(1)]);
+    });
+  });
+
+  describe("checkSignatures", () => {
+    let signersToAdd;
+
+    beforeEach("", async () => {
+      signersToAdd = [await accounts(0), await accounts(1), await accounts(2)];
+      await signers.addSigners(signersToAdd);
+    });
+
+    it("should check signatures", async () => {
+      const privateKey = Buffer.from(OWNER_PRIVATE_KEY, "hex");
+      let expectedTxHash = "0xc4f46c912cc2a1f30891552ac72871ab0f0e977886852bdd5dccd221a595647d";
+      let expextedNonce = "1794147";
+
+      let signHash = web3.utils.soliditySha3(
+        { value: "0x76e98f7d84603AEb97cd1c89A80A9e914f181679", type: "address" },
+        { value: "1", type: "uint256" },
+        { value: await accounts(0), type: "address" },
+        { value: expectedTxHash, type: "bytes32" },
+        { value: expextedNonce, type: "uint256" },
+        { value: "98", type: "uint256" },
+        { value: true, type: "bool" }
+      );
+
+      let signature = ethSigUtil.personalSign({ privateKey: privateKey, data: signHash });
+
+      await signers.checkSignatures(signHash, [signature]);
+    });
+
+    it("should revert when try duplicate signers", async () => {
+      const privateKey = Buffer.from(OWNER_PRIVATE_KEY, "hex");
+      let expectedTxHash = "0xc4f46c912cc2a1f30891552ac72871ab0f0e977886852bdd5dccd221a595647d";
+      let expextedNonce = "1794147";
+
+      let signHash = web3.utils.soliditySha3(
+        { value: "0x76e98f7d84603AEb97cd1c89A80A9e914f181679", type: "address" },
+        { value: "1", type: "uint256" },
+        { value: await accounts(0), type: "address" },
+        { value: expectedTxHash, type: "bytes32" },
+        { value: expextedNonce, type: "uint256" },
+        { value: "98", type: "uint256" },
+        { value: true, type: "bool" }
+      );
+
+      let signature = ethSigUtil.personalSign({ privateKey: privateKey, data: signHash });
+
+      await truffleAssert.reverts(
+        signers.checkSignatures(signHash, [signature, signature]),
+        "Signers: duplicate signers"
+      );
+    });
+
+    it("should revert when try sign by not signer", async () => {
+      const privateKey = Buffer.from("df57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e", "hex");
+      let expectedTxHash = "0xc4f46c912cc2a1f30891552ac72871ab0f0e977886852bdd5dccd221a595647d";
+      let expextedNonce = "1794147";
+
+      let signHash = web3.utils.soliditySha3(
+        { value: "0x76e98f7d84603AEb97cd1c89A80A9e914f181679", type: "address" },
+        { value: "1", type: "uint256" },
+        { value: await accounts(0), type: "address" },
+        { value: expectedTxHash, type: "bytes32" },
+        { value: expextedNonce, type: "uint256" },
+        { value: "98", type: "uint256" },
+        { value: true, type: "bool" }
+      );
+
+      let signature = ethSigUtil.personalSign({ privateKey: privateKey, data: signHash });
+
+      await truffleAssert.reverts(signers.checkSignatures(signHash, [signature]), "Signers: invalid signer");
+    });
+
+    it("should revert when try signers < threshold", async () => {
+      await signers.setSignaturesThreshold(1);
+      let expectedTxHash = "0xc4f46c912cc2a1f30891552ac72871ab0f0e977886852bdd5dccd221a595647d";
+      let expextedNonce = "1794147";
+
+      let signHash = web3.utils.soliditySha3(
+        { value: "0x76e98f7d84603AEb97cd1c89A80A9e914f181679", type: "address" },
+        { value: "1", type: "uint256" },
+        { value: await accounts(0), type: "address" },
+        { value: expectedTxHash, type: "bytes32" },
+        { value: expextedNonce, type: "uint256" },
+        { value: "98", type: "uint256" },
+        { value: true, type: "bool" }
+      );
+
+      await truffleAssert.reverts(signers.checkSignatures(signHash, []), "Signers: threshold is not met");
+    });
+  });
+});
