5b2c4978c46f725b8f52d4a4bc29045647091c75
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-08-04 13:37:03 +0300

    added test for withdrawERC1155

--- contracts/mocks/handlers/ERC1155HandlerMock.sol
+++ contracts/mocks/handlers/ERC1155HandlerMock.sol
@@ -3,4 +3,14 @@ pragma solidity ^0.8.9;
 
 import "../../handlers/ERC1155Handler.sol";
 
-contract ERC1155HandlerMock is ERC1155Handler {}
+contract ERC1155HandlerMock is ERC1155Handler {
+    function withdrawERC1155(
+        address token_,
+        uint256 tokenId_,
+        uint256 amount_,
+        address receiver_,
+        bool isWrapped_
+    ) external {
+        _withdrawERC1155(token_, tokenId_, amount_, receiver_, isWrapped_);
+    }
+}
--- test/handlers/ERC1155Handler.test.js
+++ test/handlers/ERC1155Handler.test.js
@@ -81,9 +81,9 @@ describe("ERC1155Handler", () => {
   describe("getERC1155SignHash", () => {
     it("should encode args", async () => {
       let expectedTxHash = "0xc4f46c912cc2a1f30891552ac72871ab0f0e977886852bdd5dccd221a595647d";
-      let expextedNonce = "1794147";
+      let expectedNonce = "1794147";
       let expectedChainId = 31378;
-      let expectedIsWraped = true;
+      let expectedIsWrapped = true;
 
       let signHash0 = await handler.getERC1155SignHash(
         token.address,
@@ -91,9 +91,9 @@ describe("ERC1155Handler", () => {
         baseAmount,
         OWNER,
         expectedTxHash,
-        expextedNonce,
+        expectedNonce,
         expectedChainId,
-        expectedIsWraped
+        expectedIsWrapped
       );
 
       assert.equal(
@@ -104,13 +104,13 @@ describe("ERC1155Handler", () => {
           { value: baseAmount, type: "uint256" },
           { value: OWNER, type: "address" },
           { value: expectedTxHash, type: "bytes32" },
-          { value: expextedNonce, type: "uint256" },
+          { value: expectedNonce, type: "uint256" },
           { value: expectedChainId, type: "uint256" },
-          { value: expectedIsWraped, type: "bool" }
+          { value: expectedIsWrapped, type: "bool" }
         )
       );
 
-      expectedIsWraped = false;
+      expectedIsWrapped = false;
 
       let signHash1 = await handler.getERC1155SignHash(
         token.address,
@@ -118,9 +118,9 @@ describe("ERC1155Handler", () => {
         baseAmount,
         OWNER,
         expectedTxHash,
-        expextedNonce,
+        expectedNonce,
         expectedChainId,
-        expectedIsWraped
+        expectedIsWrapped
       );
 
       assert.equal(
@@ -131,12 +131,51 @@ describe("ERC1155Handler", () => {
           { value: baseAmount, type: "uint256" },
           { value: OWNER, type: "address" },
           { value: expectedTxHash, type: "bytes32" },
-          { value: expextedNonce, type: "uint256" },
+          { value: expectedNonce, type: "uint256" },
           { value: expectedChainId, type: "uint256" },
-          { value: expectedIsWraped, type: "bool" }
+          { value: expectedIsWrapped, type: "bool" }
         )
       );
       assert.notEqual(signHash0, signHash1);
+    });
+  });
+
+  describe("withdrawERC721", () => {
+    it("should withdraw 100 tokens, wrapped = true", async () => {
+      await handler.depositERC1155(token.address, baseId, baseAmount, "receiver", "kovan", true);
+      await handler.withdrawERC1155(token.address, baseId, baseAmount, OWNER, true);
+
+      assert.equal(await token.balanceOf(OWNER, baseId), baseAmount);
+      assert.equal(await token.balanceOf(handler.address, baseId), "0");
+    });
+
+    it("should withdraw 52 tokens, wrapped = false", async () => {
+      await handler.depositERC1155(token.address, baseId, baseAmount, "receiver", "kovan", false);
+      await handler.withdrawERC1155(token.address, baseId, baseAmount, OWNER, false);
+
+      assert.equal(await token.balanceOf(OWNER, baseId), baseAmount);
+      assert.equal(await token.balanceOf(handler.address, baseId), "0");
+    });
+
+    it("should revert when token address is 0", async () => {
+      await truffleAssert.reverts(
+        handler.withdrawERC1155("0x0000000000000000000000000000000000000000", baseId, baseAmount, OWNER, true),
+        "ERC1155Handler: zero token"
+      );
+    });
+
+    it("should revert when amount is 0", async () => {
+      await truffleAssert.reverts(
+        handler.withdrawERC1155(token.address, baseId, 0, OWNER, true),
+        "ERC1155Handler: amount is zero"
+      );
+    });
+
+    it("should revert when receiver address is 0", async () => {
+      await truffleAssert.reverts(
+        handler.withdrawERC1155(token.address, baseId, baseAmount, "0x0000000000000000000000000000000000000000", true),
+        "ERC1155Handler: zero receiver"
+      );
     });
   });
 });
